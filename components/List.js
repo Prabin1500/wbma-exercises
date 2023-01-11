a22ed960ee09ce43857a6a989f1971538ef0e98d
prabind <prabin.dhakal@metropolia.fi>
2023-01-12 00:39:16 +0200

    task C

--- components/List.js
+++ components/List.js
@@ -1,36 +1,11 @@
-import { useEffect, useState } from 'react';
+
 import { StyleSheet, FlatList, TouchableOpacity} from 'react-native';
+import { useMedia } from '../hooks/ApiHooks';
 import {baseUrl} from '../utils/variables'
 import ListItem from './ListItem';
 
 const List = () => {
-
-  const url = 'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
-  const [mediaArray, setMediaArray] = useState([]);
-
-  const loadmedia = async() => {
-    try{
-      const response = await fetch(baseUrl + 'media');
-      const json = await response.json();
-      const media = await Promise.all(
-        json.map(async (file) => {
-          const fileResponse = await fetch(baseUrl + 'media/' + file.file_id);
-          return await fileResponse.json();
-        })
-      );
-      setMediaArray(media);
-
-    }catch(error){
-      console.error('List, loadMedia', error)
-    }
-
-  }
-
-  useEffect(() => {
-    loadmedia();
-  }, []);
-
-  console.log('List, mediaArray', mediaArray);
+  const {mediaArray} = useMedia();
 
   return(
     <FlatList
