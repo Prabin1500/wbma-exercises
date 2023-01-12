219405c19a55c242293b2533cb5e6f1d91ef5cb4
prabind <prabin.dhakal@metropolia.fi>
2023-01-12 22:42:47 +0200

    layout modified for task A

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,19 +1,48 @@
 import { Image, StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+import PropTypes from 'prop-types';
+import { Dimensions } from 'react-native'
 
-const ListItem = ({singleMedia}) =>{
+const ListItem = ({singleMedia}) => {
   const item = singleMedia;
-  return(
-    <TouchableOpacity>
+  return (
+    <TouchableOpacity style={styles.row}>
       <Image
-        style={{width: 100, height: 100}}
-        source={{uri: item.thumbnails.w160}}>
-      </Image>
-      <View>
-        <Text>{item.title}</Text>
-        <Text>{item.description}</Text>
+        style={styles.image}
+        source={{uri: item.thumbnails.w160}}
+      ></Image>
+      <View style={styles.box}>
+        <Text style={styles.listTitle}>{item.title}</Text>
+        <Text style={styles.description}>{item.description}</Text>
       </View>
     </TouchableOpacity>
   );
+};
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    backgroundColor : '#ccc',
+    marginBottom:10,
+    width: Dimensions.get('window').width,
+  },
+  box : {
+    flex : 1,
+    marginTop:20,
+  },
+  image :{
+    width:160,
+    height:250,
+    margin:10,
+  },
+  listTitle : {
+    fontWeight : 'bold',
+    fontSize : 20,
+  },
+
+});
+
+ListItem.propTypes = {
+  singleMedia: PropTypes.object,
 };
 
 export default ListItem;
