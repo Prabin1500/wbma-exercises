7fc81ec7a363d62f914a8081402e0492f7766770
prabind <prabin.dhakal@metropolia.fi>
2023-01-12 09:26:47 +0200

    Styling added

--- components/ListItem.js
+++ components/ListItem.js
@@ -3,17 +3,40 @@ import { Image, StyleSheet, View, TouchableOpacity, Text } from 'react-native';
 const ListItem = ({singleMedia}) =>{
   const item = singleMedia;
   return(
-    <TouchableOpacity>
-      <Image
-        style={{width: 100, height: 100}}
-        source={{uri: item.thumbnails.w160}}>
-      </Image>
-      <View>
-        <Text>{item.title}</Text>
+    <TouchableOpacity style={styles.row}>
+      <View style = {styles.box}>
+        <Image
+          style={styles.image}
+          source={{uri: item.thumbnails.w160}}>
+        </Image>
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.listTitle}>{item.title}</Text>
         <Text>{item.description}</Text>
       </View>
     </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    backgroundColor : '#ccc',
+    marginBottomf : 10,
+  },
+  box : {
+    flex : 1,
+    padding:10,
+  },
+  image :{
+    flex : 1,
+    minHeight: 100,
+  },
+  listTitle : {
+    fontWeight : 'bold',
+    fontSize : 20,
+    paddingBottom:15,
+  }
+});
 
 export default ListItem;
