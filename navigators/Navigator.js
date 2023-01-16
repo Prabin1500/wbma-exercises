d13fa9beeeb385a931983394c32dc54498229081
prabind <prabin.dhakal@metropolia.fi>
2023-01-16 10:04:56 +0200

    dummy login-logout button added

--- navigators/Navigator.js
+++ navigators/Navigator.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
 import Home from '../views/Home';
 import Profile from '../views/Profile';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from '../views/Login';
 import Single from '../views/Single';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import { MainContext } from '../contexts/MainContext';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -32,16 +34,24 @@ const TabScreen = () => {
 };
 
 const StackScreen = () => {
+  const {isLoggedIn} = useContext(MainContext);
   return(
     <Stack.Navigator>
-      <Stack.Screen
-        name='Tabs'
-        component={TabScreen}
-        options={{headerShown : false}} />
-      <Stack.Screen name='Single' component={Single} />
+      {isLoggedIn ? (
+        <>
+          <Stack.Screen
+            name='Tabs'
+            component={TabScreen}
+            options={{headerShown : false}} />
+          <Stack.Screen name='Single' component={Single} />
+        </>
+      ) : (
+        <Stack.Screen name='Login' component={Login}></Stack.Screen>
+      )}
+
     </Stack.Navigator>
-  )
-}
+    );
+};
 
 const Navigator = () => {
   return (
